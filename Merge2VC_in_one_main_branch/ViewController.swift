ec1bbae2b33701aa8bc7299423a5219305282acf
VLADIMIR SYLEIMANOV <bannabels@gmail.com>
2021-01-14 17:53:41 +0300

    Initial Commit

---
+++ Merge2VC_in_one_main_branch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Merge2VC_in_one_main_branch
+//
+//  Created by Vladimir Syleimanov on 14.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
